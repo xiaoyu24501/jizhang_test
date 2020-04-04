47d4cb3bed7dca79829a47672dc33631c5cca4d9
laofeionline <laofeionline@163.com>
2020-04-04 15:13:16 +0800

    首次创建

---
+++ jizhang_test/jizhang_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jizhang_test
+//
+//  Created by 费雨霏 on 2020/4/4.
+//  Copyright © 2020 老费online. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
